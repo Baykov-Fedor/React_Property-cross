260fb16e42ec425736b2f9297ae9d98a4382630f
Baykov-Fedor <baykov.f@gmail.com>
2020-12-08 17:48:21 +0300

    working on search form, made base request to API

--- src/components/search-form/search-form.component.tsx
+++ src/components/search-form/search-form.component.tsx
@@ -1,36 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import * as countries from "./regions.data.json";
 
 import "./search-form.styles.scss";
 
-function SearchForm(props) {
-  let countries = {
-    Australia: "https://api.nestoria.com.au",
-    Brasil: "https://api.nestoria.com.br",
-    Deutschland: "https://api.nestoria.de",
-    España: "https://api.nestoria.es",
-    France: "https://api.nestoria.fr",
-    India: "https://api.nestoria.in",
-    Italia: "https://api.nestoria.it",
-    Mexico: "https://api.nestoria.mx",
-    UK: "https://api.nestoria.co.uk",
+function SearchForm() {
+  let countriesNew: { [key: string]: string } = countries;
+  let [searchInput, setSearchInput] = useState("");
+  let [region, setRegion] = useState("");
+
+  const handleChange = (evt: any) => {
+    const value: string = evt.target.value;
+    const name: string = evt.target.name;
+    switch (name) {
+      case "searchInput":
+        setSearchInput(value);
+      case "region":
+        setRegion(countriesNew[value]);
+      default:
+        return;
+    }
   };
+
+  let submitForm = async (evt: any) => {
+    evt.preventDefault();
+    let url: string = `${region}/api?action=echo&encoding=json&place_name=${searchInput}`;
+    let response: any = await fetch(url);
+    if (response.ok) {
+      let json: JSON = await response.json();
+      console.log(json);
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
+  };
+
   return (
-    <div className="search-form">
-      <form name="searchForm">
-        <label htmlFor="selectCountry">Select Country:</label>
-        <select id="selectCountry">
-          {Object.keys(countries).map((item, idx) => (
-            <option key={idx} value={idx}>
-              {item}
-            </option>
-          ))}
-        </select>
-        <input
-          type="text"
-          placeholder="Type in place, post code, tube station, etc."
-        ></input>
-      </form>
-    </div>
+    <form className="search-form" onSubmit={submitForm}>
+      <label htmlFor="selectCountry">Select Country:</label>
+      <select id="selectCountry" onChange={handleChange} name="region">
+        {Object.keys(countries).map((item, idx) => (
+          <option key={idx} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Type in place, post code, tube station, etc."
+        onChange={handleChange}
+        value={searchInput}
+        name="searchInput"
+      ></input>
+      <input type="submit"></input>
+    </form>
   );
 }
 
